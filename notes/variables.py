c74b8d9fbfadaf4ca533ee2340fca6b29029a5b3
Astrolists <Xander.Parker@UCAS-edu.net>
2025-08-29 14:42:45 -0600

    Variables Notes

---
+++ notes/variables.py
@@ -0,0 +1,31 @@
+#XP 7th Variables Notes
+
+#What is one way to have your program give an output?
+#A Print Statement !!
+print("HELLLOOOO!!!!")
+
+#What is a variable?
+#A variable is anything you give a value to in coding
+#   
+# how do i assign a variable?
+# Naming a variable and using an assignment operator to tell the computer its going to give it a value, and the assignment operator is an equals sign (=). After using the assignment operator, we give the variable a value.
+
+#How do i create an input on my program?
+#to get an input from my user, i have to write "input" then add parentheses. in the parenthesis you ask a user a question.
+name = input("What is your name?")
+
+print("Hello, name")
+#How many things can be put into the same print statement?
+
+#how do i reassign a variable?
+#Simply just changing how much the variable is equal to 
+
+#Examples of variables
+School = "UCAS"
+single_digit = 4
+
+#Snake case is when all letters are lowercase and you use _ instead of spaces. Camel case is capitalizing the first letter of every word but the first one, and you remove spaces, and a pascal case is identical camel case but you capitalize the first one as well.
+how_much = 100
+
+print(how_much)
+
