a77e32cd275d88178a2f10100062c615221d557b
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-04 14:41:45 -0600

    Did my first python programming, and did expressions practice

---
+++ notes/expressions.py
@@ -0,0 +1,37 @@
+name = input("What is your name?")
+print("hello", name)
+#treyson has 12 apples, he has 5 friends to give apples to. how many apples does each friend get?
+apples = 12
+friends = 5
+print("Each friend gets", apples/friends, "apples. woah.")
+
+#ASSIGNMENT: AVERAGE AGE OF A GROUP OF 4 PEOPLE
+
+People = 4
+
+Age1 = 20
+
+Age2 = 30
+
+Age3 = 40
+
+Age4 = 40
+
+average=Age1+Age2+Age3+Age4
+print("The average age is", average)
+
+print("Addition(+):  ", Age1 + Age2)
+print("Addition(+):  ", Age1 + Age2)
+print("Addition(+):  ", Age1 + Age2)
+
+print("Subtraction(-): ", Age4-Age2)
+
+print("Multiplication(*): ", Age3*Age1)
+
+print("Divion(/): ", Age2/Age3)
+
+print("Exponents(**): ", Age3**Age4)
+
+print("Integer Division(//): ", Age4//Age1)
+
+print("Modulo(%): ", Age4%Age1)
