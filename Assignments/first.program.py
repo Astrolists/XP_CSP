46d5fee5a2f2c089ca3106d9918e073af12f230c
Astrolists <xander.parker@ucas-edu.net>
2025-09-08 19:54:43 +0000

    I did it!

--- Assignments/first.program.py
+++ Assignments/first.program.py
@@ -1,5 +1,10 @@
 #CSP FIRST PYTHON PROGRAM ASSIGNMENT
 
-name=input("Whats your name?")
-
+name = input("What is your name?")
 print("hello,", name)
+#treyson has 12 apples, he has 5 friends to give apples to. how many apples does each friend get?
+apples = 12
+friends = 5
+print("Each friend gets", apples/friends, "apples. woah.")
+
+print("I did it!")
