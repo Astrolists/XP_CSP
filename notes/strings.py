c77bac69925bf4200a72403dd5b6283489176315
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-08 14:41:47 -0600

    Started string notes

--- notes/strings.py
+++ notes/strings.py
@@ -1,1 +1,10 @@
-#STRINGS
+#XP 7th STRINGS NOTES
+first_name = input("What is your first name? \n").strip().title()
+
+last_name = input("What is your last name? \n").strip().title()
+print("Hello,", first_name, last_name)
+
+full_name = first_name + last_name
+sentence = 'the quick brown fox jumps over the lazy dog'
+
+print("Did you know that", sentence)
