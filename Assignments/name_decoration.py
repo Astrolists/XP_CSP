b6d1372d5d6ac1ac386a2d13c81f60bf86559f7d
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-10 14:39:01 -0600

    did name decoration and silly sentences

---
+++ Assignments/name_decoration.py
@@ -0,0 +1,8 @@
+#XP 7TH CSP NAME DECORATIONS
+
+username = input("What is your username? \n")
+
+decoration = "<<<<<<<<"
+
+decoration0 = ">>>>>>>>>>>>"
+print(decoration + username + decoration0)
---
+++ Assignments/silly_sentences.py
@@ -0,0 +1,15 @@
+#XP CSP 7TH SILLY SENTENCES
+
+word1 = input("Give me an adjective \n")
+
+word2 = input("Give me another adjective \n")
+
+word3 = input("Now give me a type of bird \n")
+
+word4 = input("A room in the house, too \n")
+
+word5 = input("Past tense verb \n")
+
+word6 = input("Present tense verb \n")
+
+print("It was a " + word1 + ", cold november day. I woke up to the smell of, " + word3 +  " roasting in the " + word4 + " downstairs. I " + word5 + "down the stairs to see if i could help.")
