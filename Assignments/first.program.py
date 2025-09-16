773f09155852b28f2f2c53d2ed698030c5a9441f
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-16 14:41:32 -0600

    Started update hello world

--- Assignments/first.program.py
+++ Assignments/first.program.py
@@ -1,7 +1,7 @@
 #CSP FIRST PYTHON PROGRAM ASSIGNMENT
 
 name = input("What is your name?")
-print("hello,", name)
+print("hello,", {name})
 #treyson has 12 apples, he has 5 friends to give apples to. how many apples does each friend get?
 apples = 12
 friends = 5
---
+++ Assignments/update_hello.py
@@ -0,0 +1,16 @@
+#CSP UPDATE HELLO ASSIGNMENT
+
+def hello(name):
+    name = input("What is your name? \n")
+
+
+    print(f"hello, {name}")
+
+
+name = input("What is your name? \n")
+hello(xa)
+hello(xi)
+hello(xo)
+hello(xe)
+hello(xu)
+
