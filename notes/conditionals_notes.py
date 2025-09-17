1f0b171bed06cfaa0c095777cbdae260c53956a9
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-17 14:41:45 -0600

    conditionals notes

---
+++ notes/conditionals_notes.py
@@ -0,0 +1,20 @@
+#XP CSP 7TH CONDITIONALS NOTES
+
+homework_is_done = "yes"
+
+if homework_is_done == "no":
+    print("Then go do it.")
+else:
+    print("You have permission to go have free time") 
+
+
+
+
+
+
+grade = 93
+
+if grade >= 90:
+    print(f"you have {grade}%, good job!")
+elif grade >= 80:
+    print(f"You are doing well! good job")
