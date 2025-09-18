7816085157b62f8c2ec5acebfe274d35a831fb5a
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-18 14:38:13 -0600

    did "old enough" assignment

--- notes/conditionals_notes.py
+++ notes/conditionals_notes.py
@@ -12,9 +12,29 @@ else:
 
 
 
-grade = 93
+grade = int(input("What is your grade?"))
 
 if grade >= 90:
-    print(f"you have {grade}%, good job!")
+    if grade > 100:
+        print("You cheated. Liar.")
+    else:
+        print(f"you have {grade}%, good job!")
 elif grade >= 80:
     print(f"You are doing well! good job")
+elif grade <= 79:
+    print("get your grade up. fool.")
+
+
+
+title = input("What is your title? \n")
+
+
+
+if title == "Teacher":
+    print("You are a teacher.")
+elif title == "Teachers assistant" or title == "TA":
+    print("You are a teaching assistant.")
+else:
+    print(f"hello, you are a {title}.")
+    if grade >= 60:
+        print("and you are passng the class. technically")
---
+++ practices/old_enough.py
@@ -0,0 +1,15 @@
+# XP CSP 7TH "Old Enough" ASSIGNMENT
+
+age = int(input("How old are you?"))
+if age < 3:
+    print("youre too young to do anything!")
+elif age == 15:
+    print ("You can get your learners permit!")
+elif age == 17: 
+    print ("you can get your drivers insurance!")
+elif age >= 18:
+    print ("You can vote!")
+elif age < 15 and age >= 4:
+    print ("You should be in school!")
+else:
+    print("You should be in school!") 
