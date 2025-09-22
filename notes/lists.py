426e925463a627bcda02456f97a85f250b9f3138
Astrolists <Xander.Parker@UCAS-edu.net>
2025-09-22 14:41:53 -0600

    did loops notes

--- notes/lists.py
+++ notes/lists.py
@@ -2,4 +2,8 @@
 
 siblings = ["Erika", "Davina", "Elena", ["Gabi", "Craig"]]
 
-print (siblings)
+print (siblings[0])
+print("siblings")
+siblings[0] = "john"
+siblings[1] = "jim"
+siblings[2] = "jam"
---
+++ notes/loops.py
@@ -0,0 +1,52 @@
+#XP 7th Loops Notes
+
+#Drinks = ["Milk", "Soda", "Water", "Coffee", "Tea"]
+
+#for Drinks in Drinks:
+#    print(f"{Drinks} is my favorite beverage")
+#    print("Hi!")
+
+
+#for x in range (1, 21):
+#    print(x)
+#
+
+
+
+
+#i = 0
+#while True:
+#    print(i)
+#    i+=1
+
+
+#i = 1
+
+#while i < 20:
+#    print(i)
+#    i+=1
+
+#while x < 21:
+#    if x % 2 == 0:
+#        print(f"{x} is an even number.")
+#    else:
+#        print(f"{x} is an odd number")
+#    x += 1
+
+#import random
+
+#b = 1
+#end = random.randint(1,20)
+#
+#while b != end:
+# #   print("Duck")
+#    b += 1
+
+#print("Goose")
+
+#while True:
+#    if b == end:
+#        print("Goose!")
+#        break
+#    else:
+#        print("Duck")
