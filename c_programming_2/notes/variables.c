069aea641a6096e46597b046e3d37e4dd9fa7f9a
Astrolists <xander.parker@ucas-edu.net>
2025-10-02 14:41:55 -0600

    Did stuff

---
+++ c_programming_2/notes/variables.c
@@ -0,0 +1,14 @@
+// XP 7th Variables Notes
+#include <stdio.h>
+
+int main(void){
+    int grade = 95; // 4 bytes
+    float pi = 3.14 // 4 bytes
+    double long_pi = 3.1415932653; // 8 bytes
+    char  letter_grade = 'A'; // 1 byte
+    char name[] = "larose";
+    printf("%s I did it!", name);
+    printf(" you have a %d in the class, that is an %c ", name);
+
+    return 0;
+}
