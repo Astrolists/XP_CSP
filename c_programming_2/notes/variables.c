c9de34a3916d15efb9cfd0a6ef56781dcb2b2b06
Astrolists <xander.parker@ucas-edu.net>
2025-10-03 14:41:53 -0600

    Finished notes

--- c_programming_2/notes/variables.c
+++ c_programming_2/notes/variables.c
@@ -1,14 +1,25 @@
 // XP 7th Variables Notes
-#include <stdio.h>
+#include <stdio.h> // standard library
 
 int main(void){
-    int grade = 95; // 4 bytes
-    float pi = 3.14 // 4 bytes
+
+
+
+
+
+
+    int grade; // 4 bytes
+    float pi = 3.14;// 4 bytes
     double long_pi = 3.1415932653; // 8 bytes
     char  letter_grade = 'A'; // 1 byte
-    char name[] = "larose";
-    printf("%s I did it!", name);
-    printf(" you have a %d in the class, that is an %c ", name);
+   printf("%s I did it! \n",letter_grade);
+   printf(" you have a %d in the class, that is an %c\n", letter_grade);
+    printf("what is your grade percentage as a whole number: ");
+    scanf("%d", &grade);
+
+    printf("What is your letter grade: ");
+scanf(" %c", &letter_grade);
+    printf("You have a %d, in the class. that is an %c\n", letter_grade);
 
     return 0;
 }
