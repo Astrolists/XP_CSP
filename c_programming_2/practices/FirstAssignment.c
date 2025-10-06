864f7059de4baa12a3f25988203f8346f023c1cf
Astrolists <xander.parker@ucas-edu.net>
2025-10-06 21:13:15 +0000

    First assignment finished!! ! !

--- c_programming_2/practices/FirstAssignment.c
+++ c_programming_2/practices/FirstAssignment.c
@@ -1,36 +1,17 @@
+//  XP 7th first assignment
 #include <stdio.h>
-#include <math.h>
+
 
 int main (void){
 
- char name = Xander;
+    char name = [100];
+
+    printf("What is your name?: \n");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello  %s,welcome  to your first c program \n",name)
+
+
+
     return 0;
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
