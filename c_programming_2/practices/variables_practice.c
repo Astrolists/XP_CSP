8664621e289c4a5c5b252f262a33c64e6c6a0661
Astrolists <xander.parker@ucas-edu.net>
2025-10-06 21:38:00 +0000

    started variable practice

---
+++ c_programming_2/practices/variables_practice.c
@@ -0,0 +1,25 @@
+// Xp 7th Variables Practice
+#include <stdio.h>
+
+int main (void);{
+    char name = "xander";
+    int number= 67;
+    float grade= 99.99
+    char breakfast = "nothing";
+    int year = 2025;
+    char fav_subject = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
