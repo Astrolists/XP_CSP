6d674e7b758fbee7037750ec21399d956908cee0
Astrolists <xander.parker@ucas-edu.net>
2025-10-08 14:39:15 -0600

    expression stuff

---
+++ c_programming_2/practices/expressions.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (void){
+
+int return1 = 7-24/8*4+6;
+printf("7-24/8*4+6 = %d\n", return1);
+
+int return2 = 18/3-7+2*5;
+printf("18/3-7+2*5 = %d\n", return2);
+
+int return3 = (17-6/2)+4*3;
+printf("(17-6/2)+4*3 = %d\n", return3);
+
+int return4 = -2*(1*4-2/2)+(6+2-3);
+printf("-2(1*4-2/2)+(6+2-3) = %d\n", return4);
+
+
+int return5 = 6*4/12+72/8-9;
+printf("6*4/12+72/8-9 = %d\n", return5);
+
+int return6 = -1*((3-4*7)/5)-2*24/6;
+printf("-1*[(3-4*7)/5]-2*24/6 = %d\n", return6);
+
+int return7 = (3*pow(5,2)/15)-(5-pow(2,2));
+printf("(3*pow(5,2)/15)-(5-pow(2,2)) = %d\n", return7);
+
+int return8 = pow(1,4)*pow(2,2)+pow(3,3);
+printf("pow(1,4)*pow(2,2)+pow(3,3) = %d\n", return8);
+
+int return9 = pow((22/2-2*5), 2)+pow((4-6/6),2);
+printf("pow(((22/2-2*5), 2)+pow((4-6/6)),2) = %d\n", return9);
+
+
+
+
+
+
+
+
+
+
+
+}
