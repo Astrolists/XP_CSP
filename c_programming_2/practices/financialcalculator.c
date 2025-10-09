b65df7d52b45637e23eda8918fdb30b33d73768a
Astrolists <xander.parker@ucas-edu.net>
2025-10-09 21:29:50 +0000

    finished name decoration

--- c_programming_2/practices/financialcalculator.c
+++ c_programming_2/practices/financialcalculator.c
@@ -1,2 +1,12 @@
+// Xp 7th Financial Calculator
 #include <stdio.h>
 #include <math.h>
+
+int main(void){
+    int rent_per_month;
+    int utilities_per_month;
+    
+
+
+
+}
--- c_programming_2/practices/namedecoration.c
+++ c_programming_2/practices/namedecoration.c
@@ -3,15 +3,15 @@
 #include <string.h>
 
 int main (void){
-    char username[50];
-    char decoration[3] = "<<<";
-    char decoration_2[3] = ">>>";
-    char decorated_name[50];
-    printf("Tell me your username: \n");
-    scanf("%s", username);
-    strcat(decorated_name, decoration);
-    printf("[%s]\n", decorated_name);
-    strcat(decorated_name, " ");
     
+    char username[20];
+    printf("What is your username?:\n");
+    scanf("%s", username);
+
+
+    char decorated_username[30];
+    strcat(username, decorated_username);
+
+    printf("<<<%s %s>>>", username);
 
 }
