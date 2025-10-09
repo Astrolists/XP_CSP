b044625d6e99465335dca99539a4a3b596b18040
Astrolists <xander.parker@ucas-edu.net>
2025-10-09 20:50:23 +0000

    a

--- c_programming_2/practices/variables_practice.c
+++ c_programming_2/practices/variables_practice.c
@@ -2,15 +2,26 @@
 #include <stdio.h>
 
 int main (void){
-    char name = "xander";
-    int number= 67;
-    float grade= 99.99;
-    char breakfast = "nothing";
+    char name[6] = "Xander";
+    int number = 67;
+    char grade[1] = "A";
+    char breakfast[7] = "Nothing";
     int year = 2025;
-    char fav_subject = "biology";
+    char fav_subject[7] = "Biology";
+    char favorite_color[5] = "Black";
+    char school_name[4] = "UCAS";
+    char eye_color[5] = "Brown";
+    int age = 15;
 
-
-
+    printf("%s" = name);
+    printf(number);
+    printf(grade);
+    printf(year);
+    printf(fav_subject);
+    printf(favorite_color);
+    printf(school_name);
+    printf(eye_color);
+    printf(age);
 
 
 
