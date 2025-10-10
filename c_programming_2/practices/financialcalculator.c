a0472165491fc87bbd7f85b4fdbc29dd27463a76
Astrolists <xander.parker@ucas-edu.net>
2025-10-10 02:45:51 +0000

    yes

--- c_programming_2/practices/financialcalculator.c
+++ c_programming_2/practices/financialcalculator.c
@@ -3,8 +3,28 @@
 #include <math.h>
 
 int main(void){
-    int rent_per_month;
-    int utilities_per_month;
+    float monthly_rent;
+    float monthly_utility;
+    float monthly_income;
+    float monthly_transportation;
+    float monthly_groceries;
+
+   
+    printf("What is your monthly rent?\n");
+    scanf("%f", &monthly_rent);
+
+    printf("what is your monthly utility bill?\n");
+    scanf("%f", &monthly_utility);
+
+    printf("what is your monthly income?\n");
+    scanf("%f", &monthly_income);
+
+    printf("What is your monthly transportation cost?\n");
+    scanf("%f",&monthly_transportation);
+
+    printf("What is your monthly grocery bill?");
+    scanf("%f", &monthly_groceries);
+
     
 
 
